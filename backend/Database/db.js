1574c845ba2aff09e315bc7243fc1c4dab590104
Leo-web-7 <richardo.alelo@gmail.com>
2024-11-22 14:44:34 +0700

    hg

---
+++ backend/Database/db.js
@@ -0,0 +1,28 @@
+import mysql from "mysql2/promise"
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "230523",
+    database: "attendance_db",
+});
+
+async function testConnection() {
+    try {
+        await db.getConnection(); 
+            console.log("Koneksi ke database berasil!");
+    }   catch (error) {
+        console.error("Gagal terhubung ke database:", error);
+    }
+}
+
+async function query(command, values) {
+    try {
+        const [value] = await db.query(command, values ??[]);
+        return value
+    }   catch (error) {
+        console.error(error);
+    }
+}
+
+export {query, testConnection};
