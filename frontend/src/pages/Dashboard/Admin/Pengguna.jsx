50b3c557bc885f159f8a3a9eb028914ebd4b5127
Astaroth <seanadare@gmail.com>
2024-12-01 22:23:33 +0700

    k

--- frontend/src/pages/Dashboard/Admin/Pengguna.jsx
+++ frontend/src/pages/Dashboard/Admin/Pengguna.jsx
@@ -5,11 +5,11 @@ import { useNavigate } from "react-router-dom"
 
 const Pengguna = () => {
 
-    const Navigate = useNavigate(); 
+    const Navigate = useNavigate();
 
     return (
         <div className="FadeInSection">
-        <div className="flex flex-row gap-2 items-center mb-4">
+            <div className="flex flex-row gap-2 items-center mb-4">
                 <button onClick={() => Navigate('/Dashboard')} className="flex flex-row gap-2 items-center opacity-50">
                     <p>Dashboard</p>
                     <FaChevronRight size={11} />
@@ -18,8 +18,8 @@ const Pengguna = () => {
                     <p className="text-purple-500">Users</p>
                 </div>
             </div>
-        <h2 className='font-bold'>Pengguna</h2>
-        <TableUser/>        
+            <h2 className='font-bold'>Users</h2>
+            <TableUser />
         </div>
     )
 }
