d5a091579ed4ee0d257eb877dbbdcc6e97a8ea97
Leo-web-7 <richardo.alelo@gmail.com>
2024-12-11 18:04:50 +0700

    h

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -1,10 +1,13 @@
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
+import dotenv, { config } from "dotenv";
 
-const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "230523",
-    database: "attendance_db",
+dotenv.config();
+
+export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 export async function testConnection() {
